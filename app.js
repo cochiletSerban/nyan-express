2b3556ca8f58150dacb3e44b30fce645eccb50d0
cochiletserban <cochiletserban@gmail.com>
2024-02-10 17:12:45 +0000

    http server now

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const app = express();
 const https = require('https');
 const fs = require('fs');
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/app.highlightstranslations.ro/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/app.highlightstranslations.ro/fullchain.pem', 'utf8');
+ // const privateKey = fs.readFileSync('/etc/letsencrypt/live/app.highlightstranslations.ro/privkey.pem', 'utf8');
+//const certificate = fs.readFileSync('/etc/letsencrypt/live/app.highlightstranslations.ro/fullchain.pem', 'utf8');
 
-const httpsOptions = {
+/*const httpsOptions = {
     key: privateKey,
     cert: certificate
-};
+};*/
 
 
 //Serve static files from the public directory
 app.use(express.static('public'));
 
-const server = https.createServer(httpsOptions, app);
+//const server = http.createServer(app);
 
-const PORT = 443;
-server.listen(PORT, () => {
+const PORT = 3000;
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
